fa836904288bf6bf2ef90ee0707565b69b7a6daf
megapelsh <tkachenkovladv@gmail.com>
2022-03-30 15:34:35 +0300

    .env added

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
+require ('dotenv').config()
+
 const fs = require('fs')
 const puppeter = require('puppeteer')
 
-let link = 'https://www.competencylibrary.com/';
+let link = process.env.url;
 
 (async () => {
     let flag = true
     let res = []
-    let lang = 'en'
+    let lang = ['en', 'nl', 'dk', 'de', 'fr', 'es', 'pt', 'ar', 'zh', 'pl', 'ru', 'tr']
     let counter = 0
 
     try {
         let browser = await puppeter.launch({
             headless: false,
-            slowMo: 100,
+            slowMo: 500,
             devtools: true,
         })
         let page = await browser.newPage()
@@ -20,15 +22,39 @@ let link = 'https://www.competencylibrary.com/';
             width: 1400, height: 900
         })
 
-        while ((flag)) {
-            await  page.goto(`${link}${lang}`)
-            console.log(`${link}${lang}`)
-            counter++
-            console.log(counter)
-        }
+        // отримуємо список компетенцій англійською
+        await page.goto(`${link}${lang[0]}`)
+        await page.waitForSelector('.btn-social')
+        let html = await page.evaluate(async () => {
+            let page = []
+
+            try {
+                let competensyArr = document.querySelectorAll('h4.text-semibold')
+                console.log(competensyArr)
+            }
+            catch (e) {
+                console.log(e)
+            }
+
+            return ''
+        }, {waitUntil: '.btn-social'})
+
+
+
+        // while (flag && lang[counter] !== 0) {
+        //     await  page.goto(`${link}${lang[counter]}`)
+        //
+        //     console.log(`${link}${lang[counter]}`)
+        //     console.log(lang[counter])
+        //
+        //
+        //
+        //     counter++
+        //
+        // }
     }
     catch (e) {
         console.log(e)
-        await browser.close()
+        // await browser.close()
     }
 })();
